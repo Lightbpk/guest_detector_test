ad8bb84bf476f798010111c734504a6bada18ede
Lightbpk <bpk735@gmail.com>
2021-02-17 15:52:35 +0700

    'addcounter'

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,31 @@ main() => runApp(
     textDirection: TextDirection.ltr,
     child: Container(
       color: Color(0xFFFFFFFF),
-      child: App(),
+      child: Counter(),
     ),
   ),
 );
 
-class App extends StatelessWidget {
+
+class Counter extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _CounterSate();
+
+}
+
+class _CounterSate extends State<Counter> {
+
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: GestureDetector( // используется как обычный виджет
         onTap: () { // одно из свойств GestureDetector
           // Этот метод будет вызван, когда дочерний элемент будет нажат
-          print('You pressed me');
+          setState(() {
+            ++counter;
+          });
         },
         child: Container( // нашей кнопкой будет контейнер
           decoration: BoxDecoration( // стилизуем контейнер
@@ -26,8 +38,14 @@ class App extends StatelessWidget {
           ),
           width: 80.0,
           height: 80.0,
+          child: Center(
+            child: Text('$counter',
+              style: TextStyle(fontSize: 30.0),
+            ),
+          ),
         ),
       ),
     );
   }
+
 }
